af9f53a7ff538159ee63d4a169f3fb2a6ffd8c8d
afbayonac <afbayonac@gmail.com>
2020-02-19 18:43:55 -0500

    :sparkles: Add eviroment varible support!

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,25 @@
-import secrets from './secrets'
+import { config } from 'dotenv'
 
-export const PORT = 5000
-export const TWITTER_API_KEY = secrets.twitter.apiKey
-export const TWITTER_API_SECRET_KEY = secrets.twitter.apiSecretKey
-export const TWITTER_CALLBACK = 'http://localhost:3000/login/twitter'
+// load environment variables
+config({ path: `${__dirname}/../../.env` })
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    config({ path: `${__dirname}/../../.env.development` })
+    break
+  case 'production':
+    config({ path: `${__dirname}/../../.env.production` })
+    break
+  case 'staging':
+    config({ path: `${__dirname}/../../.env.staging` })
+    break
+}
+
+
+const env = process.env
+export const PORT: number = Number(env.PORT) || 5000
+export const TWITTER_API_KEY = env.TWITTER_API_KEY || ''
+export const TWITTER_API_SECRET_KEY = env.TWITTER_API_SECRET_KEY || ''
+
+// to frontend callback
+export const TWITTER_CALLBACK = process.env.TWITTER_CALLBACK || ''
