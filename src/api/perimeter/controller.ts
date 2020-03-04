3c36915a84631780cee9db220e064cf66f5052a8
afbayonac <afbayonac@gmail.com>
2020-03-04 02:08:38 -0500

    :sparkles: Implement get user enpoint

--- src/api/perimeter/controller.ts
+++ src/api/perimeter/controller.ts
@@ -1,6 +1,18 @@
 import db from '../../database'
 import { Request, Response } from 'express'
 
-export const getPerimeter = (req: Request, res: Response): Response => {
-  return res.status(418).json({ message: 'endpoint under construction  🚧' })
+export const getPerimeter = async (req: Request, res: Response): Promise<Response> => {
+  const key = req.params.id
+  console.log(key)
+  try {
+
+    const perimeter = await db.collection('perimeters').document(key)
+    console.log(perimeter)
+    return res.status(200).json(perimeter)
+  } catch (e) {
+    if (e.isArangoError && e.code === 404) {
+      return res.status(404).json({ message: 'resource no found' })
+    }
+    throw e
+  }
 }
