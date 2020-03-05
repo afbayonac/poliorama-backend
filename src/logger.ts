7f38b2bce844ecf308c527b050d6c83fc9fa4f1e
afbayonac <afbayonac@gmail.com>
2020-03-05 12:49:51 -0500

    :sparkles: Add logger

---
+++ src/logger.ts
@@ -0,0 +1,12 @@
+import debug from 'debug'
+
+const polioramaLogger  = debug('poliorama')
+
+const logger = {
+  info: polioramaLogger.extend('info'),
+  err: polioramaLogger.extend('err'),
+  debug: polioramaLogger.extend('debug'),
+  warn: polioramaLogger.extend('warn')
+}
+
+export default logger
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
+import { createServer } from 'http'
+
 import app from './app'
 import { PORT } from './config/constants'
-import { createServer } from 'http'
+import logger from './logger'
 
 const server = createServer(app)
 
 app.on('databaseIsReady', () => {
-  server.listen(PORT, () => { console.log(`Poliorama backend is running to port ${PORT}`) })
+  server.listen(PORT, () => { logger.info(`Poliorama backend is running to port ${PORT}`) })
 })
 
 export default server
