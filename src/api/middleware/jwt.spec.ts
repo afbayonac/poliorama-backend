47ed7a3cdbcb03d0aa2bacf26ca681678d68852e
afbayonac <afbayonac@gmail.com>
2020-03-16 01:51:36 -0500

    :umbrella: Update JWT test

--- src/api/middleware/jwt.spec.ts
+++ src/api/middleware/jwt.spec.ts
@@ -15,7 +15,18 @@ describe('jwt', function () {
     const token = encode(user)
     expect(token).to.be.a.jwt
       .and.have.claim('_key', user._key)
+      .and.have.claim('picUrl', user.picUrl)
+      .and.have.claim('screenName', user.screenName)
       .and.have.claim('role', user.role)
     expect(token).signedWith(JWT_SECRET_KEY)
+  })
+
+  it('encode', function () {
+    const token = encode(user)
+    const data = decode(token)
+    expect(data).to.have.property('_key', user._key)
+    expect(data).to.have.property('picUrl', user.picUrl)
+    expect(data).to.have.property('screenName', user.screenName)
+    expect(data).to.have.property('role', user.role)
   })
 })
